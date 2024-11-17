5fcd75ba269a4959a9a2ebeb4ff1ddf6d9c0b496
jmlc643 <josemarialuyocampos@gmail.com>
2024-11-17 18:18:26 -0500

    first commit

---
+++ src/main/java/com/upao/pe/backiot/BackIotApplication.java
@@ -0,0 +1,13 @@
+package com.upao.pe.backiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackIotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackIotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/upao/pe/backiot/BackIotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upao.pe.backiot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackIotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
